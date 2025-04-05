a83b71bf9a8834e2fc11d158c2157792eeff961b
Aditya Adavade <adityawork118@gmail.com>
2025-04-05 15:47:09 +0530

    Updated signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,29 +1,28 @@
 'use client';
-
 import Link from "next/link";
 import { useRef, useState } from "react";
 import React from "react";
 
 export default function SignUp() {
-    interface userLoginType {
+    interface userSignType {
         username: string;
         email: string | any;
         password: string | number;
     }
-    const errorBlock = useRef<HTMLDivElement>(null);
-    const errorContent = useRef<HTMLSpanElement>(null);
+    const errorBlock = useRef<HTMLDivElement | null>(null);
+    const errorContent = useRef<HTMLSpanElement | null>(null);
     const passwordCheck = useRef<HTMLInputElement>(null);
-        // Password toggler
-        const togglePassword = () => {
-            if (passwordCheck.current) {
-                if (passwordCheck.current.type === "password") {
-                    passwordCheck.current.type = "text";
-                } else {
-                    passwordCheck.current.type = "password";
-                }
+    // Password toggler
+    const togglePassword = () => {
+        if (passwordCheck.current) {
+            if (passwordCheck.current.type === "password") {
+                passwordCheck.current.type = "text";
+            } else {
+                passwordCheck.current.type = "password";
             }
         }
-    const [LogValues, setLogValues] = useState<userLoginType>({
+    }
+    const [SignValues, setSignValues] = useState<userSignType>({
         username: "",
         email: "",
         password: "",
@@ -31,14 +30,29 @@ export default function SignUp() {
     // Input handler
     const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        setLogValues((prevValues) => ({
+        setSignValues((prevValues) => ({
             ...prevValues,
             [name]: value,
         }));
-        console.log(LogValues);
-
     };
-
+    // Signup Values
+    const returnSignValues = () => {
+        if (!SignValues.username || !SignValues.email || !SignValues.password) {
+            if (errorBlock.current) {
+                errorBlock.current.style.display = "block";
+                if (errorContent.current) {
+                    errorContent.current.textContent = "Please Fill the Fields!";
+                }
+            }
+        } else {
+            if (errorBlock.current) {
+                errorBlock.current.style.display = "block"
+                if (errorContent.current) {
+                    errorContent.current.textContent = "Success";
+                }
+            }
+        }
+    }
     return (
         <div className="my-42">
             <div id="error-block" ref={errorBlock} className="hidden mx-auto mb-4 w-fit border border-stone-800 rounded-lg px-2.5 py-1.5 transition-all">
@@ -50,29 +64,30 @@ export default function SignUp() {
                 <div>
                     <h2 className="text-xl text-center">Register</h2>
                 </div>
+                {/* User Inputs */}
                 <div className="flex flex-col justify-start gap-2.5 text-white">
                     <div className="flex flex-col">
                         <label htmlFor="username">Username</label>
-                        <input type="text" required id="username" name="username" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" value={LogValues.username} onChange={handleInput} />
+                        <input type="text" required id="username" name="username" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" value={SignValues.username} onChange={handleInput} />
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="email">Email</label>
-                        <input type="text" required id="email" name="email" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" value={LogValues.username} onChange={handleInput} />
+                        <input type="text" required id="email" name="email" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" value={SignValues.email} onChange={handleInput} />
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="password">Password</label>
                         <div className="flex flex-row gap-1.5 items-center">
-                            <input ref={passwordCheck} type="text" required id="password" name="password" className="border border-stone-600 focus:ring-2 focus:ring-blue-500 focus:outline-none rounded-lg px-1.5 py-0.5" value={LogValues.password} onChange={handleInput} />
+                            <input ref={passwordCheck} type="text" required id="password" name="password" className="border border-stone-600 focus:ring-2 focus:ring-blue-500 focus:outline-none rounded-lg px-1.5 py-0.5" value={SignValues.password} onChange={handleInput} />
                             <button onClick={() => togglePassword()} className="flex bg-stone-900 p-1 cursor-pointer rounded-lg"><span className="flex material-symbols-outlined">
                                 visibility_off
                             </span></button>
                         </div>
                     </div>
                     <div className="w-full">
-                        <button className="w-full bg-blue-500 text-white py-1.5 rounded-lg cursor-pointer hover:bg-blue-600">Register Me</button>
+                        <button className="w-full bg-blue-500 text-white py-1.5 rounded-lg cursor-pointer hover:bg-blue-600" onClick={() => returnSignValues()}>Register Me</button>
                     </div>
                     <div className="text-center">
-                        <p>Already a Member? <Link href="/login" prefetch={true} className="underline text-blue-600">Login</Link></p>
+                        <p>Already a Member? <Link href="/login" className="underline text-blue-600">Login</Link></p>
                     </div>
                 </div>
             </div>
