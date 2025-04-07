c12dc634cfdc4933b88dac463722cd619fef5493
Aditya Adavade <adityawork118@gmail.com>
2025-04-07 21:35:32 +0530

    Updated forgot page

--- app/forgot/page.tsx
+++ app/forgot/page.tsx
@@ -6,19 +6,30 @@ export default function Forgot() {
     interface changePassword {
         email: string | any;
     }
-    const errorContent = useRef(null);
-    const errorBlock = useRef(null);
-        const [LogValues, setLogValues] = useState<changePassword>({
-            email: ""
-        });
-        // Input handler
-        const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-            const { name, value } = e.target;
-            setLogValues((prevValues) => ({
-                ...prevValues,
-                [name]: value,
-            }));
-        };
+    const errorBlock = useRef<HTMLDivElement | null>(null);
+    const errorContent = useRef<HTMLSpanElement | null>(null);
+    const [Email, setEmail] = useState<changePassword>({
+        email: ""
+    });
+    // Input handler
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setEmail((prevValues) => ({
+            ...prevValues,
+            [name]: value,
+        }));
+    };
+    // Return OTP
+    const returnOtp = () => {
+        if (!Email.email) {
+            if (errorBlock.current) {
+                errorBlock.current.style.display = "block";
+                if (errorContent.current) {
+                    errorContent.current.textContent = "Invalid Credientials";
+                }
+                }
+        }
+    }
     return (
         <div className="my-42">
             <div id="error-block" ref={errorBlock} className="hidden mx-auto mb-4 w-fit border border-stone-800 rounded-lg px-2.5 py-1.5 transition-all">
@@ -34,10 +45,10 @@ export default function Forgot() {
                 <div className="flex flex-col justify-start gap-2.5 mt-4.5 text-white">
                     <div className="flex flex-col">
                         <label htmlFor="email">Email</label>
-                        <input value={LogValues.email} onChange={handleInput} type="text" required id="email" name="email" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" />
+                        <input value={Email.email} onChange={handleInput} type="text" required id="email" name="email" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" />
                     </div>
                     <div className="w-full">
-                        <button className="w-full bg-blue-500 text-white py-1.5 rounded-lg cursor-pointer hover:bg-blue-600">Send OTP</button>
+                        <button className="w-full bg-blue-500 text-white py-1.5 rounded-lg cursor-pointer hover:bg-blue-600" onClick={() =>  returnOtp()}>Send OTP</button>
                     </div>
                     <div className="text-center">
                         <Link href="/" className="text-md underline text-blue-500">Go Home</Link>
