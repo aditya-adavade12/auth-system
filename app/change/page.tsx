241e11abcdbe493c872198752c395d7cebca2bbb
Aditya Adavade <adityawork118@gmail.com>
2025-04-09 21:34:06 +0530

    Updated files

--- app/change/page.tsx
+++ app/change/page.tsx
@@ -32,25 +32,36 @@ export default function ChangePassword() {
             console.log(Password);
         }
     }
+    // Close the modal
+    const closeBlock = () => {
+        if (errorBlock.current) {
+            errorBlock.current.style.display = "none";
+        }
+    }
     return (
         <div className="my-42">
             <div id="error-block" ref={errorBlock} className="hidden mx-auto mb-4 w-fit border border-stone-800 rounded-lg px-2.5 py-1.5 transition-all">
-                <span ref={errorContent} className="font-medium"></span>
+                <span className="flex flex-row items-center gap-4">
+                    <span ref={errorContent} className="font-medium"></span>
+                    <button onClick={() => closeBlock()} className="flex cursor-pointer text-stone-500"><span className="flex material-symbols-outlined">
+                        close
+                    </span></button>
+                </span>
             </div>
             <title>Password</title>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-            <div id="main-wrapper" className="w-fit border border-stone-800 rounded-lg p-4 mx-auto">
+            <div id="main-wrapper" className="w-fit border border-stone-800 rounded-lg p-4 mx-auto shadow-2xl">
                 <div>
                     <h2 className="text-xl text-center">Change Password</h2>
                 </div>
                 {/* User Inputs */}
-                <div className="flex flex-col justify-start gap-2.5 mt-8.5 text-white">
+                <div className="flex flex-col justify-start gap-2.5 mt-8.5">
                     <div className="flex flex-col">
                         <label htmlFor="password">New Password</label>
                         <input value={Password.password} onChange={handleInput} type="text" required id="password" name="password" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" />
                     </div>
                     <div className="w-full">
-                        <button className="w-full bg-blue-500 text-white py-1.5 rounded-lg cursor-pointer hover:bg-blue-600" onClick={() =>  returnPassword()}>Change</button>
+                        <button className="w-full bg-blue-500 text-white py-1.5 rounded-lg cursor-pointer hover:bg-blue-600" onClick={() => returnPassword()}>Change</button>
                     </div>
                     <div className="text-center">
                         <Link href="/" className="text-md underline text-blue-500">Go Home</Link>
--- app/otp/page.tsx
+++ app/otp/page.tsx
@@ -30,22 +30,32 @@ export default function OTP() {
             }
         } else {
             console.log(userOtp);
-            
+        }
+    }
+    // Close the modal
+    const closeBlock = () => {
+        if (errorBlock.current) {
+            errorBlock.current.style.display = "none";
         }
     }
     return (
         <div className="my-42">
             <div id="error-block" ref={errorBlock} className="hidden mx-auto mb-4 w-fit border border-stone-800 rounded-lg px-2.5 py-1.5 transition-all">
-                <span ref={errorContent} className="font-medium"></span>
+                <span className="flex flex-row items-center gap-4">
+                    <span ref={errorContent} className="font-medium"></span>
+                    <button onClick={() => closeBlock()} className="flex cursor-pointer text-stone-500"><span className="flex material-symbols-outlined">
+                        close
+                    </span></button>
+                </span>
             </div>
             <title>OTP Verfication</title>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-            <div id="main-wrapper" className="w-fit border border-stone-800 rounded-lg p-4 mx-auto">
+            <div id="main-wrapper" className="w-fit border border-stone-800 rounded-lg p-4 mx-auto shadow-2xl">
                 <div>
                     <h2 className="text-xl text-center">Verify OTP</h2>
                 </div>
                 {/* User Inputs */}
-                <div className="flex flex-col justify-start gap-2.5 mt-1.5 text-white">
+                <div className="flex flex-col justify-start gap-2.5 mt-1.5">
                     <div className="flex flex-col">
                         <label htmlFor="otp">OTP</label>
                         <input value={userOtp.otp} onChange={handleInput} type="text" required id="otp" name="otp" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" />
