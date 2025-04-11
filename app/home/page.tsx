75314deb94ad9b3606c5b9ebc484c8a05cc2abc7
Aditya Adavade <adityawork118@gmail.com>
2025-04-11 23:53:28 +0530

    Updated page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
     return (
-        <div>This is home page Your login now</div>
+       <div>
+        <div className="mx-auto flex flex-col justify-center items-center gap-1.5 mt-24 rounded-lg border border-stone-800 shadow-2xl w-fit p-2">
+            <h2 className="text-4xl font-semibold">You're on Home</h2>
+            <p className="text-stone-500 font-medium">*This page only fetches when your login.</p>
+            <div>
+                <button className="hover:underline font-medium text-xl cursor-pointer text-blue-500">Logout</button>
+            </div>
+        </div>
+       </div>
     )
 }
