443cc8f3c1b01afd6864092a3f565fc8ef31ab59
Aditya Adavade <adityawork118@gmail.com>
2025-04-14 13:56:37 +0530

    Updated server with HTTPS

--- server/server.ts
+++ server/server.ts
@@ -3,22 +3,32 @@ import helmet from "helmet";
 import cors from "cors";
 import https from "https";
 import fs from "fs";
-import path from "path";
 
 
 const app = express();
 const port = 8999;
 
+// Middleware
+
+app.use(express.json());
+
 // Security Hardening
 
 app.use(helmet());
-app.use(cors({ origin: ["https://localhost:8999"] }));
+
+const corsOptions =  {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders : ["Content-Type", "Authorization"]
+}
+
+app.use(cors(corsOptions));
 
 // Securing server with HTTPS
 
 const sslOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'cert/server.key')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert/server.cert'))
+  cert: fs.readFileSync('cert/localhost.pem'),
+  key: fs.readFileSync('cert/localhost-key.pem')
 }
 
 https.createServer(sslOptions, app).listen(port, () => {
@@ -41,8 +51,12 @@ app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/api/signup", (req, res) => {
+  console.log(req.body);
+  res.send({message: "Working"});
+  
+})
+
+
 app.post("/signup", (req, res) => {});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
