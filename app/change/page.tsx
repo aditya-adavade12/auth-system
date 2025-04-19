ed44fc6c09847f2799696a7fa81c8cc95f094bf7
Aditya Adavade <adityawork118@gmail.com>
2025-04-19 20:37:19 +0530

    Updated change with apis

--- app/change/page.tsx
+++ app/change/page.tsx
@@ -1,8 +1,10 @@
 'use client';
-import Link from "next/link"
-import { useRef, useState } from "react"
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
 export default function ChangePassword() {
+    const router = useRouter();
     interface changePasswordType {
         password: number | string;
     }
@@ -19,17 +21,70 @@ export default function ChangePassword() {
             [name]: value,
         }));
     };
+    const searchParams = useSearchParams();
+    const email = searchParams.get("email");
+    useEffect(() => {
+        if (!email) router.push("/");
+    }, [router]);
     // Return a new password
-    const returnPassword = () => {
-        if (!Password.password) {
+    const btnref = useRef<HTMLButtonElement | null>(null);
+    const loginBlock = useRef<HTMLDivElement | null>(null);
+    let userObject = {
+        ...Password, 
+        email: email,
+    }
+    const returnPassword = async () => {
+        if (!Password.password || Password.password.toString().trim().length < 8) {
             if (errorBlock.current) {
                 errorBlock.current.style.display = "block";
                 if (errorContent.current) {
-                    errorContent.current.textContent = "Invalid Credientials";
+                    errorContent.current.textContent = "Invalid Password (Password length must be 8 or greater than)";
                 }
             }
         } else {
-            console.log(Password);
+            if (btnref.current) {
+                btnref.current.textContent = "Please Wait..";
+                btnref.current.style.opacity = "0.8";
+                btnref.current.disabled = true;
+            }
+            console.log(userObject);
+            
+            try {
+                let req = await fetch("https://localhost:8999/api/change", {
+                    headers: { "Content-Type": "application/json" },
+                    method: "POST",
+                    body: JSON.stringify(userObject)
+                });
+                let res = await req.json();
+                if (req.ok) {
+                    if (errorBlock.current) {
+                        errorBlock.current.style.display = "block";
+                        if (errorContent.current) {
+                            errorContent.current.textContent = "Password has been changed";
+                        }
+                    }
+                    if (loginBlock.current) {
+                        loginBlock.current.style.display = "block";
+                    }
+                    if (btnref.current) {
+                        btnref.current.textContent = "Change Password";
+                        btnref.current.style.opacity = "0.8";
+                        btnref.current.disabled = true;
+                    }
+                }
+            } catch (error) {
+                if (errorBlock.current) {
+                    errorBlock.current.style.display = "block";
+                    if (errorContent.current) {
+                        errorContent.current.textContent = "Internal Server Error! " + error;
+                    }
+                }
+                if (btnref.current) {
+                    btnref.current.textContent = "Change Password";
+                    btnref.current.style.opacity = "1";
+                    btnref.current.disabled = false;
+                }
+            }
         }
     }
     // Close the modal
@@ -61,10 +116,13 @@ export default function ChangePassword() {
                         <input value={Password.password} onChange={handleInput} type="text" required id="password" name="password" className="focus:ring-2 focus:ring-blue-500 focus:outline-none border border-stone-600 outline-none rounded-lg px-1.5 py-0.5" />
                     </div>
                     <div className="w-full">
-                        <button className="w-full bg-blue-500 text-white py-1.5 rounded-lg cursor-pointer hover:bg-blue-600" onClick={() => returnPassword()}>Change</button>
+                        <button className="w-full bg-blue-500 text-white py-1.5 rounded-lg cursor-pointer hover:bg-blue-600" ref={btnref} onClick={() => returnPassword()}>Change</button>
                     </div>
                     <div className="text-center">
                         <Link href="/" className="text-md underline text-blue-500">Go Home</Link>
+                    </div>
+                    <div ref={loginBlock} className="hidden">
+                        <Link href="/login" className="text-md underline text-blue-500">Login Now</Link>
                     </div>
                 </div>
             </div>
